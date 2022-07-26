625c1d07fdc8cbbafda8c00e00cbc8c2143af104
Denis Rybalka <denis.rubalka1@gmail.com>
2022-07-26 20:06:06 +0300

    loading & error while fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
+import React, { useContext, useEffect, useState } from "react";
+import BeatLoader from "react-spinners/BeatLoader";
+
 import axios from "axios";
-import React, { useContext, useEffect } from "react";
+import { ExchangeRateContext } from "./context";
+
 import CurrencyExchange from "./components/CurrencyExchange";
 import Header from "./components/Header";
-import { ExchangeRateContext } from "./context";
+
+import "./scss/app.scss";
 
 const API_KEY = "d5dedcc189-54c3d15f5f-rfme67";
 const API = "https://api.fastforex.io/fetch-multi?from=UAH&to=EUR,USD";
 
 const App = () => {
   const { exchangeRate, setExchangeRate } = useContext(ExchangeRateContext);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`${API}&api_key=${API_KEY}`)
-      .then(({ data }) => setExchangeRate({ ...data.results, UAH: 1 }));
+      .then(({ data }) => setExchangeRate({ ...data.results, UAH: 1 }))
+      .catch((e) => {
+        console.error("Error while fetching data!", e);
+        setError(true);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1500)
+      });
   }, []);
 
   return (
     <div className="App">
-      <Header />
-      <CurrencyExchange />
+      {loading ? (
+        <div className="loader-wrap">
+          <BeatLoader size={50} color={"lightgreen"} />
+        </div>
+      ) : (
+        <div className="content">
+          <Header />
+          <CurrencyExchange />
+        </div>
+      )}
     </div>
   );
 };
